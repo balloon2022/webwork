a40a9885da05707b2ab5e701beb4a9b34c0347e4
balloon2022 <2833437164@qq.com>
2025-08-12 18:55:17 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+/**
+ * FileName: script.js
+ * Author: WebWork
+ * CreationTime: 2025-03-02 20:47:32
+ */
+function roll(id){
+    document.getElementById(id).scrollIntoView({ behavior: "smooth" });
+}
+
+const year = new Date().getFullYear();
+document.getElementById('year').textContent=year;
+
+function download() {
+    window.location='https://www.ilanzou.com/s/W6C0tN44';
+}
+
+function TencentQQgroup() {
+    window.location='http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=_MgSZPWEJVBeYCsSDTdnEbKXYJSBz3FJ&authKey=yq2iux65IGZVBk0vBfnjHeLo0ckjF0KlTnBRtig%2BULW7yvLIBEL3IIclExWG9b5k&noverify=0&group_code=744760522';
+}
